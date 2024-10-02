d632ff02baa0960d8713f9084ad3e49a80ec35ee
clinton vilakati <vilakaticlinton@gmail.com>
2024-10-02 05:10:52 +0200

    updated base url

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const SERVER_URL   = "http://localhost/api.insuretech/index.php/";
 const DOWNLOAD_URL = "http://localhost/api.insuretech/uploads/";
-const BASE_URL     = "http://localhost/admin.insuretech/";
+//const BASE_URL     = "http://localhost/admin.insuretech/";
+const BASE_URL     = "https://clintonv12.github.io/admin.insuretech/";
 let pagename       = 'home'; // Default to 'home' page
 let TOKEN          = null;
 let ADMIN_USERNAME = null;
